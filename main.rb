36454578e4eafe90543c9140d97358abcc196dc8
Samuel ODaniels <samodan195@gmail.com>
2022-04-30 16:45:55 +0100

    Make existing comments more descriptive

--- main.rb
+++ main.rb
@@ -6,12 +6,12 @@ def ceasar_cipher(string, key)
   crypt_string = ""
   str_array = string.split("")
   str_array.each do |letter|
-    #check if it's a letter
+    #check if letter is an actual letter in the English alphabet
     unless letter.match?(/[a-zA-Z]/)
       crypt_string += letter
       next
     end
-    #check if it's uppercase
+    #check if letter is uppercase
     if letter == letter.upcase
       upper_case = true
       #change it to lowercase if it is
@@ -19,9 +19,10 @@ def ceasar_cipher(string, key)
     else
       upper_case = false
     end
-    #check its index in our alphabet array
+    #check its index in our array of alphabets
+    #and get new index based on #{key}
     index = alphabets.index(letter) - key
-    #apply uppercase if it it was originally uppercase
+    #apply uppercase to new cryptic letter if it it was originally uppercase
     if upper_case == true
       crypt_letter = alphabets[index].upcase
     else 
