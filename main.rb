33a49a3efc36980ad0695d685291db00660bc065
Samuel ODaniels <samodan195@gmail.com>
2022-05-02 11:35:38 +0100

    Change default shift direction and wrap from 'z' to 'a'
    a positive key now wraps right and negative wraps left.
    this will also change how the index is calculated and allow for wrapping
    our alphabets array.

--- main.rb
+++ main.rb
@@ -22,7 +22,8 @@ def ceasar_cipher(string, key)
     end
     #check its index in our array of alphabets
     #and get new index based on #{key}
-    index = alphabets.index(character) - key
+    #the modulo operator here serves to wrap from 'z' to 'a'
+    index = (alphabets.index(character) + key) % alphabets.length
     #apply uppercase to new cryptic letter if it it was originally uppercase
     if upper_case == true
       crypt_letter = alphabets[index].upcase
@@ -37,4 +38,4 @@ def ceasar_cipher(string, key)
 
 end
 
-p ceasar_cipher("Samodan195 egg", 3)
+p ceasar_cipher("zzz", 3)
