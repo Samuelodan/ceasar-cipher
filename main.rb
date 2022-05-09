fca355102c2238a7f57362c412c179849d1c0051
Samuel ODaniels <samodan195@gmail.com>
2022-05-09 03:34:38 +0100

    Make several changes to match Ruby style guide

--- main.rb
+++ main.rb
@@ -1,34 +1,30 @@
-#SOLUTION 1, #ASCII METHOD
+# frozen_string_literal: false
+
+# SOLUTION 1, #ASCII METHOD
 def ceasar_cipher(string, key)
-  crypt_string = ""
+  crypt_string = ''
   string.each_char do |character|
     unless character.match?(/[a-zA-Z]/)
-      #skip entire process and add character to final crypt_string if it's not a letter
+      # skip entire process and add character to final crypt_string if it's not a letter
       crypt_string += character
       next
     end
     character = character.ord
-    if character.between?("a".ord, "z".ord) #for lowercase
-      position = character - "a".ord
-      character = ((position + key) % 26) + "a".ord #26 letters in the alphabet
-    else #for uppercase
-      position = character - "A".ord
-      character = ((position + key) % 26) + "A".ord
+    if character.between?('a'.ord, 'z'.ord) # for lowercase
+      position = character - 'a'.ord
+      character = ((position + key) % 26) + 'a'.ord # 26 letters in the alphabet
+    else # for uppercase
+      position = character - 'A'.ord
+      character = ((position + key) % 26) + 'A'.ord
     end
     crypt_string << character.chr
   end
-    crypt_string
+  crypt_string
 end
-
-
-
-
-
-
 
 #######################################################################################
 
-#SOLUTION 2
+# SOLUTION 2
 # ARRAY REFERENCE METHOD
 
 # def ceasar_cipher(string, key)
@@ -60,7 +56,7 @@ end
 #     #apply uppercase to new cryptic letter if it it was originally uppercase
 #     if upper_case == true
 #       crypt_letter = alphabets[index].upcase
-#     else 
+#     else
 #       crypt_letter = alphabets[index]
 #     end
 
