60afd0f9cef79624162b4f1ab28e66f802b2d4e0
Samuel ODaniels <samodan195@gmail.com>
2022-06-27 01:02:03 +0100

    Write some more tests

--- spec/ceasar_cipher_spec.rb
+++ spec/ceasar_cipher_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe 'Ceasar_cipher' do
       expect(ceasar_cipher(string: 'zzzz', key: 3)).to eql('cccc')
     end
   end
+  describe 'Shift backward if key is negative' do
+    it 'should shift three times to the left' do
+      expect(ceasar_cipher(string: 'egg', key: -3)).to eql('bdd')
+    end
+    it 'should wrap from a to z' do
+      expect(ceasar_cipher(string: 'aaaa', key: -3)).to eql('xxxx')
+    end
+  end
 end
