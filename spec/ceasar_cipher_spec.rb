6ab370ba399c3aabdf652335b09c21706b34f7f3
Samuel ODaniels <samodan195@gmail.com>
2022-06-27 01:30:39 +0100

    Write more passing tests

--- spec/ceasar_cipher_spec.rb
+++ spec/ceasar_cipher_spec.rb
@@ -11,12 +11,25 @@ RSpec.describe 'Ceasar_cipher' do
       expect(ceasar_cipher(string: 'zzzz', key: 3)).to eql('cccc')
     end
   end
+
   describe 'Shift backward if key is negative' do
     it 'should shift three times to the left' do
       expect(ceasar_cipher(string: 'egg', key: -3)).to eql('bdd')
     end
     it 'should wrap from a to z' do
       expect(ceasar_cipher(string: 'aaaa', key: -3)).to eql('xxxx')
+    end
+  end
+
+  describe 'Support uppercase, whitespace, and non-english characters' do
+    it 'should maintain mixed cases' do
+      expect(ceasar_cipher(string: 'bAlOoN', key: 7)).to eql('iHsVvU')
+    end
+    it 'should preserve whitespace' do
+      expect(ceasar_cipher(string: 'egg yolk', key: 3)).to eql('hjj bron')
+    end
+    it 'should preserve other characters' do
+      expect(ceasar_cipher(string: 'wow! for real??', key: 3)).to eq('zrz! iru uhdo??')
     end
   end
 end
